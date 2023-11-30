9480df97fa9dd06e81480036117b03668255a802
Jamie Skinner <jamieskinner@jamies-mbp.lan>
2023-11-29 22:19:54 -0600

    split drawing out

--- ar.js
+++ ar.js
@@ -1,21 +1,19 @@
 import * as THREE from 'three'
 import { ARButton } from 'three/examples/jsm/webxr/ARButton.js';
 
+
 let camera, scene, renderer;
 let controller;
-let raycaster, intersectedPoint;
-
+let raycaster, cursor, tags = [];
+let box
 
 init();
 animate();
 
 function init() {
-
   const container = document.createElement('div');
   document.body.appendChild(container);
-
   scene = new THREE.Scene();
-
   camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 0.01, 20);
 
   const light = new THREE.HemisphereLight(0xffffff, 0xbbbbff, 3);
@@ -25,20 +23,19 @@ function init() {
   // Create a box geometry and material
   const boxGeometry = new THREE.BoxGeometry(0.5, 0.5, 0.5); // Size of the box
   const boxMaterial = new THREE.MeshPhongMaterial({ color: 0x00ff00 }); // Green color
-
-  // Create a mesh with the geometry and material
-  const box = new THREE.Mesh(boxGeometry, boxMaterial);
-
-  // Position the box (you can change x, y, z as needed)
-  box.position.set(0, 0.1, -1);
-
-  // Add the box to the scene
+  box = new THREE.Mesh(boxGeometry, boxMaterial);
+  box.position.set(0, 0.1, -2);
   scene.add(box);
 
   // Create the raycaster
   raycaster = new THREE.Raycaster();
-
-
+  // Initialize the plane with the image texture
+  const planeGeometry = new THREE.PlaneGeometry(0.2, 0.2); // Adjust size as needed
+  const texture = new THREE.TextureLoader().load('my-image.png');
+  const planeMaterial = new THREE.MeshBasicMaterial({ map: texture, side: THREE.DoubleSide, transparent: true });
+  cursor = new THREE.Mesh(planeGeometry, planeMaterial);
+  scene.add(cursor);
+  cursor.visible = false; // Initially hide the plane
   //
 
   renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
@@ -47,36 +44,18 @@ function init() {
   renderer.xr.enabled = true;
   container.appendChild(renderer.domElement);
 
-  //
-
   document.body.appendChild(ARButton.createButton(renderer));
 
-  //
-
-  const geometry = new THREE.CylinderGeometry(0, 0.05, 0.2, 32).rotateX(Math.PI / 2);
-
-
-
-
-
   function onSelect() {
-
-    const material = new THREE.MeshPhongMaterial({ color: 0xffffff * Math.random() });
-    const mesh = new THREE.Mesh(geometry, material);
-    mesh.position.set(0, 0, -1).applyMatrix4(controller.matrixWorld);
-    mesh.quaternion.setFromRotationMatrix(controller.matrixWorld);
-    scene.add(mesh);
-
+    tags.push(cursor);
+    cursor = null;
   }
 
   controller = renderer.xr.getController(0);
   controller.addEventListener('select', onSelect);
   scene.add(controller);
 
-  //
-
   window.addEventListener('resize', onWindowResize);
-
 }
 
 function onWindowResize() {
@@ -91,13 +70,10 @@ function onWindowResize() {
 //
 
 function animate() {
-
   renderer.setAnimationLoop(render);
-
 }
 
 function render() {
-
   renderer.render(scene, camera);
   updateRaycaster();
 }
@@ -107,24 +83,16 @@ function updateRaycaster() {
   raycaster.setFromCamera({ x: 0, y: 0 }, camera);
 
   // Check for intersections with the box
-  const intersects = raycaster.intersectObjects(scene.children);
+  const intersects = raycaster.intersectObjects([box]);
 
-  for (let i = 0; i < intersects.length; i++) {
-    if (intersects[i].object.geometry.type === 'BoxGeometry') {
-      // Found an intersection with the box
-      if (!intersectedPoint) {
-        // Create a sphere if it doesn't exist yet
-        const sphereGeometry = new THREE.SphereGeometry(0.05, 32, 32);
-        const sphereMaterial = new THREE.MeshBasicMaterial({ color: 0xff0000 });
-        intersectedPoint = new THREE.Mesh(sphereGeometry, sphereMaterial);
-        scene.add(intersectedPoint);
-      }
-      // Position the sphere at the intersection point
-      intersectedPoint.position.copy(intersects[i].point);
+  if (intersects.length > 0) {
+    const closestIntersection = intersects[0]; // Assuming closest intersection is what we want
 
-      break;
-    }
+    // Adjust the plane's orientation and position
+    cursor.position.copy(closestIntersection.point);
+    cursor.lookAt(closestIntersection.face.normal.add(closestIntersection.point));
+    cursor.visible = true; // Make the plane visible
+  } else {
+    cursor.visible = false; // Hide the plane if no intersection
   }
-
-
 }
